80863770ea598a465fe4c9d5bb70c641d2ee1e39
Mark Pedersen <markp@chailight.com>
2024-08-25 22:36:29 +1000

    tweaked params function

--- lib/mod.lua
+++ lib/mod.lua
@@ -61,6 +61,36 @@ local function add_txo_player()
         crow.ii.txo.cv(4,0)
     end
 
+    function player:add_params()
+        params:add_group("nb_txo", "txo", 4)
+
+        params:add_option("nb_txo/env", "env", { "off", "on" }, 1)
+        params:set_action("nb_txo/env", function(param)
+            if param == "on" then
+                crow.ii.txo.env_act(1,1)
+            else
+                crow.ii.txo.env_act(1,0)
+            end
+        end)
+
+        params:add_control("nb_txo/attack", "attack", time_ms)
+        params:set_action("nb_txo/attack", function(param)
+            crow.ii.txo.env_att(1,param)
+        end)
+
+        params:add_control("nb_txo/decay", "decay", time_ms)
+        params:set_action("nb_txo/decay", function(param)
+            crow.ii.txo.env_dec(1,param)
+        end)
+
+        params:add_control("nb_txo/wave", "wave", wave_id)
+        params:set_action("nb_txo/wave", function(param)
+            crow.ii.txo.osc_wave(1,param)
+        end)
+
+        params:hide("nb_txo")
+    end
+
     note_players["txo"] = player
 end
 
@@ -79,35 +109,6 @@ end
 -- env_trig - n/a
 
 
-function player:add_params()
-    params:add_group("nb_txo", "txo", 4)
-
-    params:add_option("nb_txo/env", "env", { "off", "on" }, 1)
-    params:set_action("nb_txo/env", function(param)
-        if param == "on" then
-            crow.ii.txo.env_act(1,1)
-        else
-            crow.ii.txo.env_act(1,0)
-        end
-    end)
-
-    params:add_control("nb_txo/attack", "attack", time_ms)
-    params:set_action("nb_txo/attack", function(param)
-        crow.ii.txo.env_att(1,param)
-    end)
-
-    params:add_control("nb_txo/decay", "decay", time_ms)
-    params:set_action("nb_txo/decay", function(param)
-        crow.ii.txo.env_dec(1,param)
-    end)
-
-    params:add_control("nb_txo/wave", "wave", wave_id)
-    params:set_action("nb_txo/wave", function(param)
-        crow.ii.txo.osc_wave(1,param)
-    end)
-
-    params:hide("nb_txo")
-end
 
 mod.hook.register("script_pre_init", "nb txo pre init", function()
     add_txo_player()
