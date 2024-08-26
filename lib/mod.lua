54a62f191205d7d51eb3a366e6b320e8c57d6d1d
Mark Pedersen <markp@chailight.com>
2024-08-26 22:47:35 +1000

    aligned to wsyn mutlivoice approach

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,4 +1,5 @@
-
+local Voice = require("lib/voice")
+local music = require 'musicutil'
 local mod = require 'core/mods'
 
 if note_players == nil then
@@ -27,72 +28,80 @@ local wave_id = controlspec.def {
     units = ''
 }
 
-local function add_txo_player()
-    local player = {
-        count = 0
-    }
+local player = {
+    allocator = Voice.new(4, Voice.LRU),
+    is_active = false,
+    notes = {},
+    modulation = 0,
+    channel_map = {0,0,0,0}
+}
 
-    function player:note_on(note, vel)
-        local v_vel = vel * 5 
-        local v8 = (note - 0)/12        
-        crow.ii.txo.cv(1,v_vel)
-        crow.ii.txo.osc(1, v8)
-        crow.ii.txo.env_trig(1,1)
-        --crow.ii.txo.tr_pulse(1)
-    end
+function player:add_params()
+    params:add_group("nb_txo", "txo", 4)
 
-    function player:note_off(note)
-        crow.ii.txo.cv(1,0)
-    end
+    params:add_option("nb_txo/env", "env", { "off", "on" }, 1)
+    params:set_action("nb_txo/env", function(param)
+        if param == "on" then
+            crow.ii.txo.env_act(1,1)
+        else
+            crow.ii.txo.env_act(1,0)
+        end
+    end)
 
-    function player:describe()
-        return {
-            name = "txo",
-            supports_bend = false,
-            supports_slew = false,
-            modulate_description = "unsupported",
-        }
-    end
+    params:add_control("nb_txo/attack", "attack", time_ms)
+    params:set_action("nb_txo/attack", function(param)
+        crow.ii.txo.env_att(1,param)
+    end)
 
-    function player:stop_all()
-        crow.ii.txo.cv(1,0)
-        crow.ii.txo.cv(2,0)
-        crow.ii.txo.cv(3,0)
-        crow.ii.txo.cv(4,0)
-    end
+    params:add_control("nb_txo/decay", "decay", time_ms)
+    params:set_action("nb_txo/decay", function(param)
+        crow.ii.txo.env_dec(1,param)
+    end)
 
-    function player:add_params()
-        params:add_group("nb_txo", "txo", 4)
+    params:add_control("nb_txo/wave", "wave", wave_id)
+    params:set_action("nb_txo/wave", function(param)
+        crow.ii.txo.osc_wave(1,param)
+    end)
 
-        params:add_option("nb_txo/env", "env", { "off", "on" }, 1)
-        params:set_action("nb_txo/env", function(param)
-            if param == "on" then
-                crow.ii.txo.env_act(1,1)
-            else
-                crow.ii.txo.env_act(1,0)
-            end
-        end)
-
-        params:add_control("nb_txo/attack", "attack", time_ms)
-        params:set_action("nb_txo/attack", function(param)
-            crow.ii.txo.env_att(1,param)
-        end)
-
-        params:add_control("nb_txo/decay", "decay", time_ms)
-        params:set_action("nb_txo/decay", function(param)
-            crow.ii.txo.env_dec(1,param)
-        end)
-
-        params:add_control("nb_txo/wave", "wave", wave_id)
-        params:set_action("nb_txo/wave", function(param)
-            crow.ii.txo.osc_wave(1,param)
-        end)
-
-        params:hide("nb_txo")
-    end
-
-    note_players["txo"] = player
+    params:hide("nb_txo")
 end
+
+function player:note_on(note, vel)
+    local v_vel = vel * 5 
+    local v8 = (note - 0)/12        
+    crow.ii.txo.cv(1,v_vel)
+    crow.ii.txo.osc(1, v8)
+    crow.ii.txo.env_trig(1,1)
+    --crow.ii.txo.tr_pulse(1)
+end
+
+function player:note_off(note)
+    crow.ii.txo.cv(1,0)
+end
+
+function player:describe()
+    return {
+        name = "txo",
+        supports_bend = false,
+        supports_slew = false,
+        modulate_description = "unsupported",
+    }
+end
+
+function player:stop_all()
+    crow.ii.txo.cv(1,0)
+    crow.ii.txo.cv(2,0)
+    crow.ii.txo.cv(3,0)
+    crow.ii.txo.cv(4,0)
+end
+
+-- local function add_txo_player()
+--    local player = {
+--        count = 0
+--    }
+    
+--    note_players["txo"] = player
+-- end
 
 -- cv_slew
 -- cv_qt
@@ -111,5 +120,5 @@ end
 
 
 mod.hook.register("script_pre_init", "nb txo pre init", function()
-    add_txo_player()
+   note_players["txo"] = player 
 end)
